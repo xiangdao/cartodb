0857e948a86c1e94ec73882f07429c73ccbebe93
Sandro Santilli <strk@keybit.net>
2013-09-27 15:31:37 +0200

    Refactor testcase forCDB_CartodbfyTable using a checker function

--- lib/sql/test/CDB_CartodbfyTableTest.sql
+++ lib/sql/test/CDB_CartodbfyTableTest.sql
@@ -1,31 +1,140 @@
 SET client_min_messages TO warning;
 \set VERBOSITY terse;
 
--- t1: table with single non-geometrical column
-CREATE TABLE t(a int);
-SELECT CDB_CartodbfyTable('t');
-SELECT f_table_schema,f_table_name,f_geometry_column,coord_dimension,srid,type
-  FROM geometry_columns
-  ORDER BY f_table_name,f_geometry_column;
-INSERT INTO t(the_geom) values ( CDB_LatLng(2,1) );
-SELECT
-  't1',
-  cartodb_id,
-  round(extract(minutes from created_at - now())) as created,
-  round(extract(minutes from updated_at - now())) as updated,
-  round(st_x(the_geom)) as geom_x,
-  round(st_y(the_geom)) as geom_y,
-  round(st_x(the_geom_webmercator)) as mercator_x,
-  round(st_y(the_geom_webmercator)) as mercator_y
-FROM t;
+CREATE OR REPLACE FUNCTION CDB_CartodbfyTableCheck(tabname regclass, label text)
+RETURNS text AS
+$$
+DECLARE
+  sql TEXT;
+  id INTEGER;
+  rec RECORD;
+  lag INTERVAL;
+  tmp INTEGER;
+  ogc_geom geometry_columns; -- old the_geom record in geometry_columns
+  ogc_merc geometry_columns; -- old the_geom_webmercator record in geometry_columns
+BEGIN
+
+  -- Save current constraints on geometry columns, if any
+  ogc_geom = ('','','','',0,0,'GEOMETRY'); 
+  ogc_merc = ogc_geom; 
+  sql := 'SELECT gc.* FROM geometry_columns gc, pg_class c, pg_namespace n '
+    || 'WHERE c.oid = ' || tabname::oid || ' AND n.oid = c.relnamespace'
+    || ' AND gc.f_table_schema = n.nspname AND gc.f_table_name = c.relname'
+    || ' AND gc.f_geometry_column IN ( ' || quote_literal('the_geom')
+    || ',' || quote_literal('the_geom_webmercator') || ')';
+  FOR rec IN EXECUTE sql LOOP
+    IF rec.f_geometry_column = 'the_geom' THEN
+      ogc_geom := rec;
+    ELSE
+      ogc_merc := rec;
+    END IF;
+  END LOOP;
+
+  PERFORM CDB_CartodbfyTable(tabname); 
+
+  sql := 'INSERT INTO ' || tabname::text || '(the_geom) values ( CDB_LatLng(2,1) ) RETURNING cartodb_id';
+  EXECUTE sql INTO STRICT id;
+  sql := 'SELECT created_at,updated_at,the_geom_webmercator FROM '
+    || tabname::text || ' WHERE cartodb_id = ' || id;
+  EXECUTE sql INTO STRICT rec;
+
+  -- Check created_at and updated_at at creation time
+  lag = rec.created_at - now();
+  IF lag > '1 second' THEN
+    RAISE EXCEPTION 'created_at not defaulting to now() after insert [ valued % ago ]', lag;
+  END IF;
+  lag = rec.updated_at - now();
+  IF lag > '1 second' THEN
+    RAISE EXCEPTION 'updated_at not defaulting to now() after insert [ valued % ago ]', lag;
+  END IF;
+
+  -- Check the_geom_webmercator trigger
+  IF round(st_x(rec.the_geom_webmercator)) != 111319 THEN
+    RAISE EXCEPTION 'the_geom_webmercator X is % (expecting 111319)', round(st_x(rec.the_geom_webmercator));
+  END IF;
+  IF round(st_y(rec.the_geom_webmercator)) != 222684 THEN
+    RAISE EXCEPTION 'the_geom_webmercator Y is % (expecting 222684)', round(st_y(rec.the_geom_webmercator));
+  END IF;
+
+  -- Check CDB_TableMetadata entry
+  sql := 'SELECT * FROM CDB_TableMetadata WHERE tabname = ' || tabname::oid;
+  EXECUTE sql INTO STRICT rec;
+  lag = rec.updated_at - now();
+  IF lag > '1 second' THEN
+    RAISE EXCEPTION 'updated_at in CDB_TableMetadata not set to now() after insert [ valued % ago ]', lag;
+  END IF;
+
+  -- Check geometry_columns entries
+  tmp := 0;
+  FOR rec IN
+    SELECT
+      CASE WHEN gc.f_geometry_column = 'the_geom' THEN 4326
+           ELSE 3857 END as expsrid,
+      CASE WHEN gc.f_geometry_column = 'the_geom' THEN ogc_geom.type
+           ELSE ogc_merc.type END as exptype, gc.*
+    FROM geometry_columns gc, pg_class c, pg_namespace n 
+    WHERE c.oid = tabname::oid AND n.oid = c.relnamespace
+          AND gc.f_table_schema = n.nspname AND gc.f_table_name = c.relname
+          AND gc.f_geometry_column IN ( 'the_geom', 'the_geom_webmercator')
+  LOOP
+    tmp := tmp + 1;
+    -- Check SRID constraint
+    IF rec.srid != rec.expsrid THEN
+      RAISE EXCEPTION 'SRID of % in geometry_columns is %, expected %',
+        rec.f_geometry_column, rec.srid, rec.expsrid;
+    END IF;
+    -- Check TYPE constraint didn't change
+    IF rec.type != rec.exptype THEN
+      RAISE EXCEPTION 'type of % in geometry_columns is %, expected %',
+        rec.f_geometry_column, rec.type, rec.exptype;
+    END IF;
+    -- check coord_dimension ?
+  END LOOP;
+  IF tmp != 2 THEN
+      RAISE EXCEPTION '% entries found for table % in geometry_columns, expected 2', tmp, tabname;
+  END IF;
+
+  -- Check GiST index 
+  sql := 'SELECT a.attname, count(ri.relname) FROM'
+    || ' pg_index i, pg_class c, pg_class ri, pg_attribute a, pg_opclass o'
+    || ' WHERE i.indrelid = c.oid AND ri.oid = i.indexrelid'
+    || ' AND a.attrelid = ri.oid AND o.oid = i.indclass[0]'
+    || ' AND a.attname IN ( ' || quote_literal('the_geom')
+    || ',' || quote_literal('the_geom_webmercator') || ')'
+    || ' AND ri.relnatts = 1 AND o.opcname = '
+    || quote_literal('gist_geometry_ops_2d')
+    || ' AND c.oid = ' || tabname::oid
+    || ' GROUP BY a.attname';
+  RAISE NOTICE 'sql: %', sql;
+  EXECUTE sql;
+  GET DIAGNOSTICS tmp = ROW_COUNT;
+  IF tmp != 2 THEN
+      RAISE EXCEPTION '% gist indices found on the_geom and the_geom_webmercator, expected 2', tmp;
+  END IF;
+
+  -- Check null constraint on cartodb_id, created_at, updated_at
+  SELECT count(*) FROM pg_attribute a, pg_class c WHERE c.oid = tabname::oid
+    AND a.attrelid = c.oid AND NOT a.attisdropped AND a.attname in
+      ( 'cartodb_id', 'created_at', 'updated_at' )
+    AND NOT a.attnotnull INTO strict tmp;
+  IF tmp > 0 THEN
+      RAISE EXCEPTION 'cartodb_id or created_at or updated_at are missing not-null constraint';
+  END IF;
+
+  RETURN label || ' cartodbfied fine';
+END;
+$$
+LANGUAGE 'plpgsql';
+
+-- table with single non-geometrical column
+CREATE TABLE t AS SELECT 1::int as a;
+SELECT CDB_CartodbfyTableCheck('t', 'single non-geometrical column');
 DROP TABLE t;
 
--- t1: table with existing srid-unconstrained (but type-constrained) the_geom
+-- table with existing srid-unconstrained (but type-constrained) the_geom
 CREATE TABLE t AS SELECT ST_SetSRID(ST_MakePoint(0,0),4326)::geometry(point) as the_geom;
-SELECT CDB_CartodbfyTable('t');
-SELECT f_table_schema,f_table_name,f_geometry_column,coord_dimension,srid,type
-  FROM geometry_columns
-  ORDER BY f_table_name,f_geometry_column;
+SELECT CDB_CartodbfyTableCheck('t', 'srid-unconstrained the_geom');
 DROP TABLE t;
 
 
+DROP FUNCTION CDB_CartodbfyTableCheck(regclass, text);
